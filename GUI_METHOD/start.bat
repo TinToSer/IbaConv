33a3268b2b7d8f66fa0d3cabbf66bbeceb9abaaa
TinToSer <52107530+TinToSer@users.noreply.github.com>
2023-12-11 09:51:34 +0100

    Parquet inclusion and possibility to pass timebase

--- GUI_METHOD/start.bat
+++ GUI_METHOD/start.bat
@@ -1,4 +1,3 @@
 @echo off
-REM https://github.com/TinToSer/IbaConv
-IbaFileConv --ibapath="C:\Program Files\iba\ibaAnalyzer\ibaAnalyzer.exe" --SourceFolder=""
+ibafileconv --ibapath="C:\Program Files\iba\ibaAnalyzer\ibaAnalyzer.exe" --SourceFolder="C:\Users\z004b2pd\Downloads\ibb" --once=True --timebase=0.001 --avgSamples=True
 pause
